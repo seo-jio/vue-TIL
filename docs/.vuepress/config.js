a47251c56b57229c61ede8097bb120592cfd2a8d
seo-jio <lio8625@naver.com>
2022-04-01 22:29:56 +0900

    ga

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,6 +34,21 @@ module.exports = {
       "meta",
       { name: "apple-mobile-web-app-status-bar-style", content: "black" },
     ],
+    [
+      "script",
+      {
+        async: true,
+        src: "https://www.googletagmanager.com/gtag/js?id=G-X8STMYZ8KD",
+        // GA에서 발급받은 코드
+      },
+    ],
+    [
+      "script",
+      {},
+      [
+        "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-X8STMYZ8KD');",
+      ],
+    ],
   ],
 
   /**
